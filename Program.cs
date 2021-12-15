ffd51f19e2310279fc51d398d3e3ceefeb7c2c04
j624364 <49117678+j624364@users.noreply.github.com>
2021-12-15 08:16:08 +0000

    First commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+void writeValue(int val)
+{
+}
+
+void writeUsage()
+{
+	Console.WriteLine("Usage: ./BLight");
+}
+
+if (args.Length == 0)
+{
+	writeUsage();
+}
+else if (args[0] == "inc")
+{
+	Console.WriteLine("Increment");
+}
+else if (args[0] == "dec")
+{
+	Console.WriteLine("Decrement");
+}
+else
+{
+	writeUsage();
+}
+
